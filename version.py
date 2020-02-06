daea6e494787b5c15a51531f8c01986f329d0f66
Bray <bsst@M-C02ZC53WLVDQ.nordstrom.net>
2020-02-06 12:17:44 -0800

    patch: testing

--- version.py
+++ version.py
@@ -2,50 +2,27 @@ import json
 import sys
 import re
 
-PATCH = 'PATCH:'
-MAJOR = 'MAJOR:'
-MINOR = 'MINOR:'
-x = 0
-y = 1
-z = 2
+merge_message = sys.argv[1]
+pattern = r'^Merge branch \'.+\' (.*) See merge request.*$'
+commit_message = re.search(pattern, merge_message).group(1).lower()
+prefix = re.search(r'^(.*):.*$', commit_message).group(1)
 
-def main():
-    merge_message = sys.argv[1]
-    pattern = r'^Merge branch \'.+\' (.*) See merge request.*$'
-    commit_message = re.search(pattern, merge_message).group(1)
+print('commit message: "' + commit_message + '"')
+print('prefix: "' + prefix + '"')
 
-    print('commit message: "' + commit_message + '"')
+package_file = open('./package.json', 'r+')
+package_json = json.loads(package_file.read())
 
-    package_file = open('./package.json', 'r+')
-    package_json = json.loads(package_file.read())
-    
+version = package_json['version']
+print('previous version: "' + version + '"')
 
-    version = str(package_json['version'])
-    print('previous version: "' + version + '"')
-    # xyz = version.split('.') ## split version x.y.z into [x,y,z]
-    # print(prefix)
+ind = 'majorminorpatch'.find(prefix) / 5
+semvar = version.split('.')
+semvar[ind] = str(int(semvar[ind]) + 1)
 
-    # ## determines which has changed, updates version accordingly
-    # if prefix.upper() == PATCH:
-    #     z_version = int(xyz[z]) + 1
-    #     xyz[z] = str(z_version)
-    # elif prefix.upper() == MINOR:
-    #     y_version = int(xyz[y]) + 1
-    #     xyz[y] =  str(y_version)
-    # elif prefix.upper() == MAJOR:
-    #     x_version = int(xyz[x]) + 1
-    #     xyz[x] =  str(x_version)
-    # else:
-    #     print('Version is not updated.')
-    #     exit(1)
+package_json['version'] = '.'.join(semvar)
+print('new version: "' + package_json['version'] + '"')    
 
-    # ## write back to package.json
-    # version = '.'.join(xyz)
-    # data['version'] = version
-    # with open(file_path, 'w') as package_file:
-    #     json.dump(data, package_file, indent = 4)
-    #     # package_file.close()
-
-
-if __name__ == '__main__':
-    main()
+package_file.seek(0)
+json.dump(package_json, package_file, indent=4)
+package_file.close()
