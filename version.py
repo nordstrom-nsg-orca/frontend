f1d559efb68c18ec9157d4c91bf005cad7b37203
Bray <bsst@M-C02ZC53WLVDQ.nordstrom.net>
2020-02-06 13:32:22 -0800

    patch: testing

--- version.py
+++ version.py
@@ -2,34 +2,27 @@ import json
 import sys
 import re
 
-print sys.argv
-
 merge_message = sys.argv[1]
-# print merge_message
-# print merge_message.strip()
+commit_message = re.search(r'^.*\n\n(.*)\n\n.*$', merge_message).group(1)
+prefix = re.search(r'^(.*):.*$', commit_message).group(1).lower()
 
-print re.search(r'^.*\n\n(.*)\n\n.*$', merge_message).group(1)
+print('commit message: "' + commit_message + '"')
+print('prefix: "' + prefix + '"')
 
-# commit_message = merge_message.split('\\n\\n')[1].lower()
-# prefix = re.search(r'^(.*):.*$', commit_message).group(1)
+package_file = open('./package.json', 'r+')
+package_json = json.loads(package_file.read())
 
-# print('commit message: "' + commit_message + '"')
-# print('prefix: "' + prefix + '"')
+version = package_json['version']
+print('previous version: "' + version + '"')
 
-# package_file = open('./package.json', 'r+')
-# package_json = json.loads(package_file.read())
+ind = 'majorminorpatch'.find(prefix) / 5
+semvar = version.split('.')
+semvar[ind] = str(int(semvar[ind]) + 1)
 
-# version = package_json['version']
-# print('previous version: "' + version + '"')
+package_json['version'] = '.'.join(semvar)
+print('new version: "' + package_json['version'] + '"')    
 
-# ind = 'majorminorpatch'.find(prefix) / 5
-# semvar = version.split('.')
-# semvar[ind] = str(int(semvar[ind]) + 1)
-
-# package_json['version'] = '.'.join(semvar)
-# print('new version: "' + package_json['version'] + '"')    
-
-# package_file.seek(0)
-# json.dump(package_json, package_file, indent=4)
-# package_file.close()
+package_file.seek(0)
+json.dump(package_json, package_file, indent=4)
+package_file.close()
 
