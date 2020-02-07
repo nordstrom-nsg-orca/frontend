c625be56deabebdfdd0c8ac6feb8adbd7b6b86e8
Bray <bsst@M-C02ZC53WLVDQ.nordstrom.net>
2020-02-06 16:23:56 -0800

    PATCH: cleaned up version.py

--- version.py
+++ version.py
@@ -3,7 +3,6 @@ import sys
 import re
 
 merge_message = sys.argv[1]
-print type(merge_message)
 commit_message = re.search(r'^.*\n\n(.*)\n\n.*$', merge_message).group(1)
 prefix = re.search(r'^(.*):.*$', commit_message).group(1).lower()
 
@@ -12,15 +11,15 @@ print('prefix: "' + prefix + '"')
 
 package_file = open('./package.json', 'r+')
 package_json = json.loads(package_file.read())
-
 version = package_json['version']
+
 print('previous version: "' + version + '"')
 
 ind = 'majorminorpatch'.find(prefix) / 5
 semvar = version.split('.')
 semvar[ind] = str(int(semvar[ind]) + 1)
-
 package_json['version'] = '.'.join(semvar)
+
 print('new version: "' + package_json['version'] + '"')    
 
 package_file.seek(0)
