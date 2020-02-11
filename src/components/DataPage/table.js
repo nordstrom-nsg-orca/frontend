b11dad6e2ba2061a4fd6589fdf552cad18b6eca9
Bray <bsst@M-C02ZC53WLVDQ.nordstrom.net>
2020-02-11 15:42:45 -0800

    PATCH: fixed lint

--- src/components/DataPage/table.js
+++ src/components/DataPage/table.js
@@ -38,9 +38,7 @@ class DataTable extends React.Component {
 
                 {this.props.headers.map((column, index) =>
                   <TableCell size='small' key={index} className={classes.tablePaper}>
-                    {row[column.column_name] != null &&
-                      row[column.column_name].toString()
-                    }
+                    {row[column.column_name] != null && row[column.column_name].toString()}
                   </TableCell>)}
 
                 {this.props.handleAction &&
